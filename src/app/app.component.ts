aefd2d245423cf30e26ff4b42d3fc776aa1ad5ac
Sarah Grimm <sarahagrimm@gmail.com>
2018-06-07 23:51:15 -0700

    answered type questions and completed practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  number: number = 1;
+  string: string = '';
+  boolean : boolean = true;
+  null = null;
+  u: undefined = undefined;
 }
