33fa23db90a68baf97d26b1734d066fadcaf40c3
Sarah Grimm <sarahagrimm@gmail.com>
2018-06-08 13:18:30 -0700

    added classes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,22 @@
 import { Component } from '@angular/core';
 
+class CarBrand{
+  protected make: string;
+  constructor(make: string){
+    console.log(this.make);
+    this.make = make;
+  }
+}
+class  Car extends CarBrand{
+  constructor(public make: string, public model: string, public year: string){
+    super(make);
+  }
+
+  getDetails(){
+    return (this.make, this.model, this.year);
+  }
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +24,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  ford: CarBrand = new CarBrand('Ford');
+  dodge: CarBrand = new CarBrand('Dodge');
+  honda: Car = new Car ('Honda', 'Civic', 1989);
 }
