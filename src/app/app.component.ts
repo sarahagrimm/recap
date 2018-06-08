bcb61661cf762ab32e05374232276addd6d4d0ba
Sarah Grimm <sarahagrimm@gmail.com>
2018-06-08 15:15:51 -0700

    added Vehicle module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,20 @@
 import { Component } from '@angular/core';
 
+module Vehicle {
+   export class Sedan {
+    make: string;
+    model: string;
+    year: number;
+
+    constructor(make: string, model: string, year: number){
+      this.make = make;
+      this.model = model;
+      this.year = year;
+    }
+  }
+
+  export const honda = new Sedan('Honda', 'Civic', 2018);
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +22,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  ford = new Vehicle.Sedan('Ford', 'Focus', 2015);
+  honda = {make: 'honda', model: 'accord', year: 2017};
+  hon = Vehicle.honda;
+  ngOnInit(){
+    console.log(this.ford);
+
+  }
+
 }
