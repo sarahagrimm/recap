85aa61ccd45b6f634836d16c3609e5ebddfbee85
Sarah Grimm <sarahagrimm@gmail.com>
2018-06-08 16:56:03 -0700

    added components and ngIf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { GreetingMessageComponent } from './greeting-message/greeting-message.component';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GreetingMessageComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/greeting-message/greeting-message.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+
+@Component ({
+ selector: 'greeting-message',
+ templateUrl: './greeting-message.component.html',
+ styleUrls: ['./greeting-message.component.css']
+})
+
+export class GreetingMessageComponent {
+
+}
